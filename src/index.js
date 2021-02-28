3d825fde1552c169808acee38b1df77f9e0b39de
Nadezhda Nedezhda <56599396+thecoocko@users.noreply.github.com>
2021-02-28 16:28:12 +0200

    fix: solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  return Array.isArray(array) && array.length !== 0 ? Math.min(...array) : 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  return Array.isArray(array) && array.length !== 0 ? Math.max(...array) : 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (Array.isArray(array) && array.length !== 0) {
+    let s = 0;
+    for (let i = 0; i < array.length; i++) {
+      s += array[i];
+    }
+    return s / array.length;
+  } else return 0;
 }
+
